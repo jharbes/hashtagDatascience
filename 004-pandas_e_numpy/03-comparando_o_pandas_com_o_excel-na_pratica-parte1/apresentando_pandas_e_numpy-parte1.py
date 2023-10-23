6b5316e04ad78d4b49c2e09fbbe785eb604431d6
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-23 12:23:43 -0300

    03 numpy pandas inicio

---
+++ 004-pandas_e_numpy/03-comparando_o_pandas_com_o_excel-na_pratica-parte1/apresentando_pandas_e_numpy-parte1.py
@@ -0,0 +1,30 @@
+"""
+# NumPy e Pandas
+
+
+- São **bibliotecas** do Python voltadas para Ciência de Dados
+
+
+- O **NumPy** é a principal biblioteca de **computação numérica** do Python, **otimizada para calculos pesados** e serve de **base para o Pandas**
+
+    - No NumPy vamos trabalhar com **arrays**, que são estruturas de dados multidimensionais (1D, 2D, 3D,...)
+    - Os arrays lembram muito as **listas**, porém com algumas diferenças e várias vantagens na questão de processamento 
+
+    
+- O **Pandas** vai ser a biblioteca que vamos utilizar em toda a parte inicial do nosso processo de Ciência de Dados! Além de possuir **alta performance e fácil uso** para trabalharmos com estruturas de dados, possui um **visual muito amigável** ao usuário não técnico
+
+    - Os DataFrames do Pandas lembram muito o excel, popularmente conhecido nas empresas
+    - É usado **desde a importação da base até a criação do modelo**, incluindo a **análise e visualização dos dados, tratamentos, agregações, etc**
+
+- Exemplo de um DataFrame do Pandas:
+
+<img src="https://drive.google.com/uc?id=1IXohvu2-EA3QoOFNUB_K9MSIerIwal_B" style='width: 9000px;' />
+
+"""
+
+# - **Podemos começar comparando as funções do Excel com o Pandas**
+#   - https://pandas.pydata.org/docs/
+
+# Importando o pandas
+import pandas as pd
+
