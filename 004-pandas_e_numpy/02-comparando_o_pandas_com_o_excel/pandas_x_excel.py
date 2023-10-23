bfc3b793d3ae5ed3d3b817cd977bb2a2e6b0a155
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-23 10:54:20 -0300

    Create pandas_x_excel.py

---
+++ 004-pandas_e_numpy/02-comparando_o_pandas_com_o_excel/pandas_x_excel.py
@@ -0,0 +1,15 @@
+"""
+
+Nomenclaturas Pandas:
+
+Excel   = Tabela
+Pandas  = Dataframe
+
+Excel   = Coluna
+Pandas  = Series
+
+Excel   = Pode ter tipos de dados diferentes em uma mesma coluna
+Pandas  = Possui apenas um tipo de dado por coluna
+
+
+"""
