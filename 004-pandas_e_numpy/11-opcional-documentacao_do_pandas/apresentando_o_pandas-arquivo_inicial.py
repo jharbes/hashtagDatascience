07d514dedbc79caf5d40e9ceebcce9d0578c37f9
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-25 07:43:18 -0300

    pandas 11 commit 2

---
+++ 004-pandas_e_numpy/11-opcional-documentacao_do_pandas/apresentando_o_pandas-arquivo_inicial.py
@@ -0,0 +1,16 @@
+"""
+# O Pandas
+- https://pandas.pydata.org/
+
+
+### O DataFrame no pandas
+- https://pandas.pydata.org/docs/getting_started/intro_tutorials/01_table_oriented.html
+
+"""
+
+# Importando o pandas
+import pandas as pd
+
+
+
+# Criando um dataframe
