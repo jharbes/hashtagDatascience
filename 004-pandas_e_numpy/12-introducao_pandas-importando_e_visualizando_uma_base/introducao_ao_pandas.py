d7e8d23cffa2999b4339127cf7b07ea1e3d9104a
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-26 10:08:53 -0300

    Create introducao_ao_pandas.py

---
+++ 004-pandas_e_numpy/12-introducao_pandas-importando_e_visualizando_uma_base/introducao_ao_pandas.py
@@ -0,0 +1,16 @@
+"""
+# Pandas
+
+
+- Alta performance e fácil uso para trabalhar com dados
+
+- Tem muitas coisas que lembram o Excel
+
+- O DataFrame é usado em várias outras bibliotecas de Ciência de Dados
+- https://pandas.pydata.org/
+
+- O Pandas vai desde a importação da base, passando por toda a análise exploratória até a exibição de gráficos
+
+"""
+# Importando o pandas
+import pandas as pd
