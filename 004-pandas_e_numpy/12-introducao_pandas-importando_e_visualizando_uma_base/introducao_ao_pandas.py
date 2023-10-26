689e69d06efdeab05937ab09a3cbb8cab6ad0fcb
Jorge Harbes <98434001+jharbes@users.noreply.github.com>
2023-10-26 10:20:06 -0300

    12 pandas commit final

--- 004-pandas_e_numpy/12-introducao_pandas-importando_e_visualizando_uma_base/introducao_ao_pandas.py
+++ 004-pandas_e_numpy/12-introducao_pandas-importando_e_visualizando_uma_base/introducao_ao_pandas.py
@@ -14,3 +14,35 @@
 """
 # Importando o pandas
 import pandas as pd
+
+
+
+"""
+### Conseguimos importar praticamente todas as bases de dados usando o pandas
+"""
+base = pd.read_csv("07_pandas.csv")
+
+
+
+### E também visualizar todas as informações sobre essa base
+# Visualizando as 5 primeiras linhas
+print(base.head())
+
+# Visualizando as 15 primeiras linhas
+display(base.head(15))
+
+
+
+# Visualizando as 5 últimas linhas
+print(base.tail())
+
+
+
+# Tamanho da base
+base.shape # (7100, 10)
+
+
+
+# Visualizando primeiras e últimas linhas e tamanho da base (display funciona apenas no jupyter)
+# display(base)
+print(base)
